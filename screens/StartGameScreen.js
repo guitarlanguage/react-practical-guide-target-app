b01a46569e0228015782742b3d2999e6b268c0a3
guitarlanguage <guitarlanguageseries@gmail.com>
2022-11-22 21:14:11 -0600

    textInput field configuration, keyboardType prop

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput 
+        style={styles.numberInput} 
+        maxLength={2} 
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -19,7 +25,7 @@ const styles = StyleSheet.create({
     // flex: 1,
     marginTop: 100,
     padding: 16,
-    backgroundColor: "blue",
+    backgroundColor: '#72063c',
     //android only property
     elevation: 4,
     //get around this with ios by using shadow
@@ -27,5 +33,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 4 },
     shadowRadius: 6,
     shadowOpacity: .5
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 });
