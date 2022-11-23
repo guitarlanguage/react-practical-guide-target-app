81f1f2df62f0cc6a2795d4ed1f52dc71f7362e76
guitarlanguage <guitarlanguageseries@gmail.com>
2022-11-22 20:33:36 -0600

    learned how to add box shadow in react native. use elevation for android and shadow for ios

--- App.js
+++ App.js
@@ -12,9 +12,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    // flex: 1,
+    backgroundColor: 'orange',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+     
   },
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,20 @@ function StartGameScreen() {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    //flex 1 helps it take up as much space as it can
+    // flex: 1,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "blue",
+    //android only property
+    elevation: 4,
+    //get around this with ios by using shadow
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: .5
+  }
+});
